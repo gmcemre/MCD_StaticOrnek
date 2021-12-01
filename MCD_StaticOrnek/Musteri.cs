272826273bd0a14c748060a1b852c77f9fc5b685
emre gemici <gmcc.emree@gmail.com>
2021-12-01 18:11:14 +0300

    Add project files.

---
+++ MCD_StaticOrnek/Musteri.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_StaticOrnek
+{
+    public class Musteri
+    {
+        #region Sanal Database
+        static ArrayList musteriDatabase;
+        #endregion
+
+        #region Static Yapıcı Metot
+        static Musteri()
+        {
+            musteriDatabase = new ArrayList();
+        }
+        #endregion
+
+        #region Field
+        public int musteriId { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public string emailAdres { get; set; }
+        public string kullaniciAdi { get; set; }
+        public int sifre { get; set; }
+
+        #endregion
+
+        #region Kapsulleme
+        private string _kullaniciAdi;
+        public string KullaniciAdi
+        {
+            get
+            {
+                return this._kullaniciAdi;
+            }
+            set
+            {
+                bool kullaniciAdiKontrol = musteriKullaniciAdiKontrol(value);
+                if (kullaniciAdiKontrol )
+                {
+                    Console.WriteLine("Eklemek istediğiniz kullanıcı adı sistem içerisinde kayıtlı.");
+                    this._kullaniciAdi = string.Empty;
+                }
+                else
+                {
+                    this._kullaniciAdi = value;
+                }
+            }
+        }
+        #endregion
+
+        private bool musteriKullaniciAdiKontrol(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        
+    }
+}
---
+++ MCD_StaticOrnek/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_StaticOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Müşteri adında bir nesne oluşturalım ve oluşturmuş olduğumuz nesnemizin içerisinde static olarak bir ArrayList oluşturalım.
+             *Oluşturmuş olduğumuz ArrayList koleksiyonumuzu sanal bir database mantığında kullanacağız.
+             *Müşteri sınıfımızın içerisine ;
+             *ID,Isim,Soyisim,EmailAdres,KullaniciAdi,Sifre isimleri ile field'lar oluşturalım.
+             *Oluşturmuş olduğumuz bu field listesinden bazılarının kapsülleme filtreleri aşağıdaki gibi olmalıdır.
+             *Kullanici adı değeri sanal olarak oluşturmuş olduğumuz database imiz içerisinde aranan eğer kullanıcı adı sanal database içerisinde bulunursa field içerisine eklenmek istenen değer eklenemesin.(Kapsülleme)
+             *Müşteri ekle adında static metot oluşturalım parametre olarak müşteri tipi alsın içerisinde ;
+             *Müşteri nesnesinin null olup olmadığını ve müşteri nesnesi içerisndeki kullanıcı adının dolu olması şartına baksın.
+             *Yukarıdaki kriterlerden geçerse müşterimizi sanal olarak oluşturuğumuz database mize eklemeden önce email adresinden bir kontrol sağlasın eğere sistemde aynı email adresi ile müşteri değeri var ise eklemesin yok ise sanal database içerisine ilgili nesneyi eklesin.
+             */
+        }
+    }
+}
